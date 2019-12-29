296c750731df2af58fbe3bf490dc06a88646defd
Matheus Lazarin <lazarin.matheus@gmail.com>
2019-12-29 17:26:20 -0300

    Moving layout to hoc folder.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
-import Aux from "../../hoc/Auxiliar"
+import Aux from "../Auxiliar"
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.module.css";
 
 class Layout extends Component {
