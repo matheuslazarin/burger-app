6bc9dd2eb41c10ffb20af0b096be25c275f9301a
Matheus Lazarin <lazarin.matheus@gmail.com>
2020-02-02 12:38:01 -0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from './containers/Checkout/Checkout';
+import Checkout from "./containers/Checkout/Checkout";
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route path='/checkout' component={Checkout}/>
+            <Route path='/' component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     )
