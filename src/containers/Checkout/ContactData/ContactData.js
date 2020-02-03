e8cd44236172292fda5cc5399623335d2fbd9b0a
Matheus Lazarin <lazarin.matheus@gmail.com>
2020-02-02 21:21:40 -0300

    Add validation to create submit input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -53,7 +53,7 @@ class ContactData extends Component {
         elementConfig: {
           options: [
             {value: 'fastest', displayValue: 'Fastest'},
-            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},
           ],
         },
         value: ''
