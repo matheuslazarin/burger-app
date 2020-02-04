132d6380960181fbd91386b64f442a54f07db623
Matheus Lazarin <matheus@inovadora.com.br>
2020-02-04 12:52:16 -0300

    Add redux and create initial redux setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 const app = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
