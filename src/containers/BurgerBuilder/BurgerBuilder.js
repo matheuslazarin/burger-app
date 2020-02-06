9c2bd1e1a1364d55ee8acc20055b5f80cc014576
Matheus Lazarin <matheus@inovadora.com.br>
2020-02-06 13:02:08 -0300

    Fix disable button problem

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -28,15 +27,15 @@ class BurgerBuilder extends Component {
     //   });
   }
 
-  updatePurchaseState = (ingredients) => {
-    const sum = Object.keys(ingredients)
+  updatePurchaseState = () => {
+    const sum = Object.keys(this.props.ings)
       .map(igKey => {
-        return ingredients[igKey];
+        return this.props.ings[igKey];
       })
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({purchasable: sum > 0});
+    return sum > 0;
   };
 
   purchaseHandler = () => {
@@ -81,7 +80,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             price={this.props.price}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState()}
             purchase={this.purchaseHandler}
           />
         </Aux>);
