eefb269a77318eb5a9db1b4271c54e8e57f073db
Matheus Lazarin <matheus@inovadora.com.br>
2020-02-07 12:53:56 -0300

    Finish redux integration

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import {connect} from 'react-redux';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 
@@ -100,7 +101,7 @@ class ContactData extends Component {
       formData[formElementId] = this.state.orderForm[formElementId].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
     };
@@ -187,4 +188,11 @@ class ContactData extends Component {
   }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  }
+};
+
+export default connect(mapStateToProps)(ContactData);
