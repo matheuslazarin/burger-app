b5d1b287d83d0c76dd0f1b63305c9d3219bfbf0d
Matheus Lazarin <lazarin.matheus@gmail.com>
2020-02-09 17:26:42 -0300

    Add redux dev tools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {createStore} from 'redux';
 import reducer from './store/reducer';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
   <Provider store={store}>
