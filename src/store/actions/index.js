2ac67e8d98ac23a0a0fb829665d47337884f52ae
Matheus Lazarin <lazarin.matheus@gmail.com>
2020-06-04 21:36:19 -0300

    Add side-effects from order to Redux Saga.

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -11,6 +11,12 @@ export {
   purchaseBurger,
   purchaseInit,
   fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+  fetchOrdersStart,
+  fetchOrdersSuccess,
+  fetchOrdersFail,
 } from './order';
 export {
   auth,
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,6 +2,7 @@ import {takeEvery} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import {logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga} from './auth';
 import {initIngredientsSaga} from './burgerBuilder';
+import {fetchOrdersSaga, purchaseBurgerSaga} from './order';
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
@@ -12,4 +13,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
